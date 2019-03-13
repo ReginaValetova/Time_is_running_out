62752099a58896575705e0510ca39d47e4c19238
reginPekin <reginbegin@gmail.com>
2019-03-14 02:32:37 +0300

    for loop

--- Functions.js
+++ Functions.js
@@ -1,4 +1,4 @@
-function countdown(time) {
+function countdown(time, elementClass) {
     // Set the date we're counting down to
     var countDownDate = new Date(time).getTime();
     // Update the count down every 1 second
@@ -16,14 +16,28 @@ function countdown(time) {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
     // Display the result in the element with id="demo"
-    document.getElementById("task--countdown").innerHTML = days + "d " + hours + "h "
+    document.getElementById(elementClass).innerHTML = days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ";
     // If the count down is finished, write some text 
     if (distance < 0) { 
         clearInterval(x); 
-        document.getElementById("task--countdown").innerHTML = "EXPIRED";
+        document.getElementById(elementClass).innerHTML = "EXPIRED";
     } 
     }, 1000); 
 } 
 
-countdown("Jan 5, 2021 15:37:25"); 
+function outputTimer() {
+    var times = ["March 14, 2019 14:30:25",
+    time2 = "March 15, 2019 14:30:20",
+    time3 = "March 16, 2019 14:50:05",
+    time4 = "March 17, 2019 14:10:15",
+    time5 = "March 18, 2019 14:33:55",
+    time6 = "March 19, 2019 14:37:25"]
+
+    for (let i = 0; i < 6; i++) {
+        countdown(times[i], "task--countdown"+i);
+    }
+
+}
+
+outputTimer();
